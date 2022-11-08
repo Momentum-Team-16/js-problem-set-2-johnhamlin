11a4b7efd61443969ed5503513c878e55b97e7b3
John Hamlin <johnhamlin@gmail.com>
2022-11-08 14:56:06 -0500

    Complete 1-5

--- homework.js
+++ homework.js
@@ -5,18 +5,33 @@
 //
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
+const remove = (arr, elementToRemove) => {
+  return arr.filter(e => e !== elementToRemove);
+};
 
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+const sum = arr => arr.reduce((a, b) => a + b, 0);
 
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+const average = arr => {
+  if (arr.length) {
+    return sum(arr) / arr.length;
+  }
+};
 
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+const minimum = arr => {
+  if (arr.length) {
+    return arr.reduce((min, next) => Math.min(min, next));
+  }
+};
+console.log(minimum([2, 1, 3]));
 
 // 6. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
